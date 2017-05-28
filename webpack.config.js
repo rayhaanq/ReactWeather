d3bc666d48456d823ec3f9f5901ecce0cc05949f
Rayhaan Quazi <rayhaan.quazi@gmail.com>
2017-05-28 17:24:30 +0100

    added css/sass loaders and added bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 
 module.exports = {
-  entry: './app/app.jsx',
+  entry: {
+    app: './app/app.jsx'
+},
   output: {
     path: path.resolve(__dirname),
     filename: './public/bundle.js'
@@ -30,9 +36,46 @@ module.exports = {
         },
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', "sass-loader"],
+          publicPath: "/public"
+        })
       }
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'source-map',
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    "window.jQuery": "jquery",
+    Tether: "tether",
+    "window.Tether": "tether",
+    Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+    Button: "exports-loader?Button!bootstrap/js/dist/button",
+    Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+    Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+    Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+    Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+    Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+    Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+    Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+    Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+    Util: "exports-loader?Util!bootstrap/js/dist/util",
+  }),
+    new ExtractTextPlugin({
+      filename: "./public/main.css",
+      disable: false,
+      allChunks: true
+
+    }),
+    new webpack.ProvidePlugin({
+      "window.Tether": "tether"
+    })
+]
 
 };
